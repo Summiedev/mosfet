441703a2d378d260221dfad2181e093846683762
maryam-shotunde <maryam.dev.engineer@gmail.com>
2026-03-26 11:42:42 +0100

    completed fastapi auth and patient models

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,27 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
-
+from datetime import datetime
 
 # USER AUTHENTICATION SCHEMAS
-class CreateUser(BaseModel):
+class CreateUser(BaseModel):   #FOR STAFF LOGIN
     username : str
     email : EmailStr
-    password : str
-
+    password : int
+   
 class UserResponse(BaseModel):
     id : str
     username : str
     email : EmailStr
+    email : EmailStr
+   
+class CreatePatient(BaseModel):    #patient login
+     username : str
+     age : int
+     gender : str
+     scan_type : str   #e.g ultrasound
+     reason : str #The reason for doing the scan
+
+class PatientResponse(CreatePatient):
+    id : int
+    time_created : datetime
+    status : str 
