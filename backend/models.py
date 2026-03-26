425a413268094aac39ff2e63f1fe6604542812ed
maryam-shotunde <maryam.dev.engineer@gmail.com>
2026-03-26 21:08:37 +0100

    Adding scan sessions and transcript

--- backend/models.py
+++ backend/models.py
@@ -6,13 +6,13 @@ from datetime import datetime
 class CreateUser(BaseModel):   #FOR STAFF LOGIN
     username : str
     email : EmailStr
-    password : int
+    password : str
    
 class UserResponse(BaseModel):
     id : str
     username : str
     email : EmailStr
-    email : EmailStr
+    
    
 class CreatePatient(BaseModel):    #patient login
      username : str
@@ -22,6 +22,33 @@ class CreatePatient(BaseModel):    #patient login
      reason : str #The reason for doing the scan
 
 class PatientResponse(CreatePatient):
-    id : int
+    id : str
     time_created : datetime
     status : str 
+
+class ChecklistItem(BaseModel):  #SCAN SESSION SCHEMA
+    item_description: str
+    is_checked: bool = False
+
+class ChecklistUpdate(BaseModel):
+    is_checked: bool = False
+
+class ScanSessionCreate(BaseModel):
+    patient_id : str
+    scan_type : str        #EXAMPLE -> XRAY, CT SCAN
+
+class UpdateTranscript(BaseModel):
+    text:str    #voice to text
+
+class ScanSessionResponse(BaseModel):
+    patient_id : str
+    scan_type : str
+    id : str
+    status : str = 'in progress'
+    transcript : list[str] = []
+    checklist : list[ChecklistItem] = []
+    time_created : datetime
+
+class ReportUpdate(BaseModel):
+    findings : str
+    status : str    
